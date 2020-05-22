937f1f370d10ad9ed02986197ea8c50e815e08a6
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-21 22:21:01 -0500

    Fancy text colors

--- records/run_all.sh
+++ records/run_all.sh
@@ -5,19 +5,15 @@ export PYTHONPATH=$PCM_DIR:$PYTHONPATH
 
 
 
-
-
-echo "format input elutions"
-#bash records/record_format_elutions.sh
+echo -e "\e[32mFormat input elutions (records/record_format_elutions.sh)"
+#bash records/record_format_elutions.sh &> logs/format_elutions.log
 
 # TODO: Add script to perform formatting check
 
-
+echo -e "\e[34mLogged at logs/format_elutions.log\e[39m"
 if [ -f "$(find "elutions/processed_elutions" -name "*.wide" | head -1)" ] 
 then    
 
-    #echo elutions/processed_elutions/*.feat
-    #ls elutions/processed_elutions/*.wide
     echo -e "\e[35mElutions formatted (elutions/processed_elutions/*.wide)\e[39m"
 
 else
@@ -25,16 +21,16 @@ else
     echo -e "\e[91mExiting\e[39m"
     exit 1
 fi
-#
+echo ""
 
 #####################
-echo "create correlation commands, default 5 replications with poisson noise, use up to 100 for final calculations"
+echo -e "\e[36mCreate correlation commands (records/record_corr_commands_gen.sh)\ndefault 5 replications with poisson noise, use up to 100 for final calculation\e[39m"
 bash records/record_corr_commands_gen.sh $PCM_DIR 5
 
-echo "run correlation commands"
+echo -e "\e[32mRun correlation commands (records/record_corr_COMMANDs.sh)"
 cat records/record_corr_COMMANDS.sh | parallel -j30 &> logs/corr_commands.log
 
-
+echo -e "\e[34mLogged at logs/corr_commands.log\e[39m"
 if [ -f "$(find "elutions/processed_elutions" -name "*.feat" | head -1)" ] 
 then    
 
@@ -47,14 +43,16 @@ else
     echo -e "\e[91mExiting\e[39m"
     exit 1
 fi
+echo ""
 #####################
 
 #####################
-echo "make alphabetization commands"
+echo -e "\e[36mMake alphabetization commands (records/record_alphabetize_commands_gen.sh"
 bash records/record_alphabetize_commands_gen.sh $PCM_DIR
 
-echo "run alphabetization commands"
+echo -e "\e[32mRun alphabetization commands (records/record_alphabetize_COMMANDS.sh"
 cat records/record_alphabetize_COMMANDS.sh | parallel -j30 &> logs/alphabetize_commands.log 
+echo -e "\e[34mLogged at logs/alphabetize_commands.log\e[39m"
 
 if [ -f "$(find "elutions/processed_elutions" -name "*.feat.ordered" | head -1)" ] 
 then    
@@ -66,12 +64,15 @@ else
     exit 1
 fi
 exit 1
+echo ""
 ####################
 
-echo "make rescale commands for distance features"
+
+
+echo -e "\e[36mMmake rescale commands for distance feature\e[39m"
 bash records/record_rescale_commands_gen.sh $PCM_DIR
 
-echo "run rescale commands for distance features"
+echo -e "\e[32mRun rescale commands for distance feature\e[39m"
 cat records/record_rescale_COMMANDS.sh | parallel -j30  &> logs/rescale_commands.log 
 
 if [ -f "$(find "elutions/processed_elutions" -name "*.feat.rescale.ordered" | head -1)" ] 
@@ -88,24 +89,24 @@ fi
 ####################
 
 
-echo "get feature names"
+echo -e "\e[32mget feature name\e[39m"
 bash records/record_get_feature_names.sh
 
 
-echo "build feature matrix"
+echo -e "\e[32mbuild feature matrix"
 bash records/record_build_feature_matrix.sh $PCM_DIR feature_matrix/features.txt 
 
 
 exit 1
-echo "format gold standards"
+echo -e "\e[32mformat gold standard\e[39m"
 bash records/record_gold_standards.sh $PCM_DIR $EXP_PREFIX $GOLD_COMPLEXES
 
-echo "feature matrix procession"
+echo -e "\e[32mfeature matrix procession"
 bash records/record_feature_matrix_processing.sh  
 
-echo "feature selection"
+echo -e "\e[32mfeature selection"
 bash records/record_feature_selection.sh  
 
 
-echo "training"
+echo -e "\e[36mtraining"
 bash records/record_training.sh 
