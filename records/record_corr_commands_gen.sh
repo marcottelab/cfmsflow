9dccf88ac6eb5adfb5d261c6033c1f0fa014c24d
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-24 22:05:30 -0500

    More error checking and adding source

--- records/record_corr_commands_gen.sh
+++ records/record_corr_commands_gen.sh
@@ -1,13 +1,30 @@
-PCM_DIR=$1
-REPS=$2
+CONFIG=$1
+source $CONFIG
 
-# 5 reps for test, 
-# 100 reps for final iteration
-for exp in elutions/processed_elutions/*.wide
+###################
+#Code
+
+while read exp
         do for corr in pearsonR spearmanR euclidean braycurtis #xcorr
-            do echo "python $PCM_DIR/protein_complex_maps/features/ExtractFeatures/canned_scripts/extract_features.py --format csv -f $corr -r poisson_noise -i $REPS $exp"
-            done
-        done >  records/record_corr_COMMANDS.sh
+
+           do
+               if [ $ADD_POISSON_NOISE == "T" ]; then
+
+                  if [[ -z $POISSON_REPS ]]; then
+                      echo 'POISSON_REPS undefined in config file. ex. POISSON_REPS=5'
+                      exit 1
+                  fi
+
+
+                   echo "python $PCM_DIR/protein_complex_maps/features/ExtractFeatures/canned_scripts/extract_features.py --format csv -f $corr -r poisson_noise -i $POISSON_REPS $exp > logs/log_corr_${exp##*/}_${corr} 2> logs/error_corr_${exp##*/}_${corr}"
+               else
+                   echo "python $PCM_DIR/protein_complex_maps/features/ExtractFeatures/canned_scripts/extract_features.py --format csv -f $corr $exp > logs/log_corr_${exp##*/}_${corr} 2> logs/error_corr_${exp##*/}_${corr}"
+ 
+               fi
+           
+
+           done
+        done < $EXPLIST >  records/record_corr_COMMANDS.sh
 
 
 
