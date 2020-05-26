e1c53c54195a47334f6fe2473e11ec4c2198e961
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-26 16:27:17 -0500

    move pipelines scripts to scripts

--- records/record_get_feature_names.sh
+++
@@ -1,5 +0,0 @@
-ls elutions/processed_elutions/*pearsonR*ordered > feature_matrix/features.txt
-ls elutions/processed_elutions/*spearmanR*ordered >> feature_matrix/features.txt
-#ls elutions/processed_elutions/*xcorr*ordered >> feature_matrix/features.txt
-ls elutions/processed_elutions/*braycurtis*rescale.ordered >> feature_matrix/features.txt
-ls elutions/processed_elutions/*euclidean*rescale.ordered >> feature_matrix/features.txt
--- scripts/pipeline_features.sh
+++ scripts/pipeline_features.sh
@@ -1,6 +1,13 @@
 set -e
 CONFIG=$1
 echo $CONFIG 
+
+if [ -f $CONFIG ]; then
+   echo "Please provide a config file as the first command line argument to this script"
+   echo "Ex. bash scripts/pipeline_training.sh config.sh"
+   exit 1
+fi
+
 source $CONFIG
 cat $CONFIG
 
