648661959373bad6bb3af569d4068f81013d4270
Claire McWhite <cmcwhite@marccomp01.ccbb.utexas.edu>
2020-05-28 20:59:10 -0500

    Move scripts up

---
+++ pipeline_training.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+set -e
+CONFIG=$1 
+
+if [ ! -f $CONFIG ]; then
+   echo "Please provide a config file"
+   exit 1
+fi
+
+source $CONFIG
+cat $CONFIG
+export PYTHONPATH=$PCM_DIR
+
+
+if [ "$RUN_GET_FEAT_FILE" == "T" ]; then
+    ./scripts/cfmsinfer-getfeatnames $CONFIG
+fi
+
+if [ "$RUN_BUILD_FEATMAT" == "T" ]; then
+    ./scripts/cfmsinfer-build $CONFIG
+fi
+
+if [ "$RUN_GOLDSTANDARD" == "T" ]; then
+    ./scripts/cfmsinfer-gold $CONFIG
+fi
+
+if [ "$RUN_LABEL_FEATMAT" == "T" ]; then
+    ./scripts/cfmsinfer-label $CONFIG
+fi
+
+if [ "$RUN_TRAIN" == "T" ]; then
+    ./scripts/cfmsinfer-train $CONFIG
+fi
