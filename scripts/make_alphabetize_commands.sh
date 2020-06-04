69b9c4215c0cda006decdc189249fc9ea2e52d02
Claire McWhite <cmcwhite@marccomp02.ccbb.utexas.edu>
2020-06-04 12:27:26 -0500

    Add chunksize to alphabetize

--- scripts/make_alphabetize_commands.sh
+++ scripts/make_alphabetize_commands.sh
@@ -10,7 +10,7 @@ while read exp
         rm -f logs/log_alphabetize_${feat##*/}
         rm -f errors/error_error_alphabetize_${exp##*/}
  
-        echo "python $PCM_DIR/protein_complex_maps/features/alphabetize_pairs.py --feature_pairs $feat --outfile ${feat}.ordered --sep ','  > logs/log_alphabetize_${feat##*/} 2> errors/error_alphabetize_${feat##*/}" 
+        echo "python $PCM_DIR/protein_complex_maps/features/alphabetize_pairs2.py --feature_pairs $feat --outfile ${feat}.ordered --sep ',' --chunksize $CHUNKSIZE  > logs/log_alphabetize_${feat##*/} 2> errors/error_alphabetize_${feat##*/}" 
       done
 
   done < $EXPLIST > records/record_alphabetize_COMMANDS.sh 
