fbef9f1663eb926c9554dabad7bbacbe33a0081b
Rachael Cox <rachaelcox@utexas.edu>
2021-08-18 15:40:30 -0500

    make dirs consistent

--- test_output/pipeline.py
+++ test_output/pipeline.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.ensemble import ExtraTreesClassifier, RandomForestClassifier
 from sklearn.feature_selection import RFE
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
@@ -11,10 +11,10 @@ features = tpot_data.drop('target', axis=1)
 training_features, testing_features, training_target, testing_target = \
             train_test_split(features, tpot_data['target'], random_state=None)
 
-# Average CV score on the training set was: 0.15574895839572261
+# Average CV score on the training set was: 0.4559605209605209
 exported_pipeline = make_pipeline(
-    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.8, n_estimators=100), n_features_to_select=2, step=0.7500000000000001),
-    ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.3, min_samples_leaf=4, min_samples_split=12, n_estimators=100)
+    RFE(estimator=ExtraTreesClassifier(criterion="entropy", max_features=0.9000000000000001, n_estimators=100), n_features_to_select=1, step=0.45),
+    RandomForestClassifier(bootstrap=True, criterion="entropy", max_features=0.6000000000000001, min_samples_leaf=3, min_samples_split=3, n_estimators=100)
 )
 
 exported_pipeline.fit(training_features, training_target)
